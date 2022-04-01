81b49301ba1a0371b82d44cf1e64c4af08ce7a38
Camilo ERodriguez <crodrigr@gmail.com>
2022-04-01 15:41:05 -0500

    Organización directorios

--- Fuentes/Back/Backend/mintic2022.unab.edu.co.c4-g28.facturador/mintic2022.unab.edu.co.c4-g28.facturador/src/main/java/mintic2022/unab/edu/co/c4g28/facturador/models/dao/IFacturaDao.java
+++
@@ -1,9 +0,0 @@
-package mintic2022.unab.edu.co.c4g28.facturador.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import mintic2022.unab.edu.co.c4g28.facturador.models.entites.Factura;
-
-public interface IFacturaDao extends CrudRepository<Factura, Long> {
-
-}
